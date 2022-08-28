debb3b3f19b00dd7bff0a0c5d0b7c440dadd6294
JacotaDiana <ana-diana.jacota@student.tuiasi.ro>
2022-08-28 11:34:49 +0300

    feat(trafficEnvironment): add trafficEnvironment class

---
+++ Disertatie/Disertatie/TrafficEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ActressMas;
+
+namespace Disertatie
+{
+    class TrafficEnvironment : EnvironmentMas
+    {
+        public TrafficEnvironment()
+        {
+        }
+    }
+}
